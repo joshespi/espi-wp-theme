54f9329d641ff5cd528c2fe186c8967d8356ce3e
Josh Espinoza <joshespi@gmail.com>
2024-05-25 17:28:15 -0600

    refactor: Improve index.php template structure and readability

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
-<?php get_header(); ?>
-
-
 <?php
-if (is_singular()) {
+get_header();
+
+$is_singular = is_singular();
+$is_front_page = is_front_page();
+
+// ob_start();
+
+if ($is_singular) {
     the_title('<h1>', '</h1>');
 } else {
     echo "<h1>Blog</h1>";
 }
 
-?>
-<?php
 // Start “The Loop”
 if (have_posts()) :
-    if (is_singular()) {
-        echo '<section class="single">';
-    } else {
-        echo '<section class="multi">';
-    }
+    echo '<section class="' . ($is_singular ? 'single' : 'multi') . '">';
     while (have_posts()) : the_post();
         echo '<article id="post-' . get_the_ID() . '" ';
         post_class();
         echo '>';
-        if (is_front_page()) {
+        if ($is_front_page) {
             the_title('<h2><a href="' . get_permalink() . '">', '</a></h2>');
         }
         if (has_post_thumbnail()) { // check if the post has a Post Thumbnail assigned to it.
@@ -29,7 +27,7 @@ if (have_posts()) :
             the_post_thumbnail();  // display the featured image.
             echo '</div>';
         }  // end Featured Image
-        if (is_singular()) {
+        if ($is_singular) {
             the_content();
             if (comments_open() || get_comments_number()) {
                 comments_template();
@@ -55,6 +53,8 @@ else :
     _e('Sorry, no  posts matched your criteria.', 'espi-theme');
 endif;
 // End of “The Loop”
-?>
 
-<?php get_footer(); ?>
+// $output = ob_get_clean();
+// echo $output;
+
+get_footer();
