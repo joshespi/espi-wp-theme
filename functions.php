b57ff0acfdd96f356ba1d739fd5f886e019ca2bd
Josh Espinoza <joshespi@gmail.com>
2024-05-25 17:08:12 -0600

    refactor: Add support for wp-block-styles, responsive-embeds, align-wide, and html5 elements

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@
 add_theme_support("post-thumbnails");
 add_theme_support("title-tag");
 add_theme_support('automatic-feed-links');
+add_theme_support("wp-block-styles");
+add_theme_support("responsive-embeds");
+add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+add_theme_support("align-wide");
+
+
 
 //add theme assets
 function espitheme_assets()
