2203cfce998155120c9e3014101a194bd9c6ddff
Josh Espinoza <joshespi@gmail.com>
2024-05-25 13:39:25 -0600

    refactor menu display to check for menu, give class if menu has more than 5, and displays a message if no menu is selected

--- footer.php
+++ footer.php
@@ -1,12 +1,24 @@
-</main>
+</footer>
 <footer>
     <nav>
+
         <?php
-        wp_nav_menu(array(
-            'theme_location' => 'footer-menu',
-            'menu_id' => 'footer-menu',
-            'menu_class' => 'footer'
-        ));
+        $menu_name = 'footer-menu';
+        $menu_items = wp_get_nav_menu_items($menu_name);
+        $menu_class = 'footer-menu';
+        if ($menu_items && count($menu_items) > 5) {
+            $menu_class .= ' moreThan5';
+        }
+
+        if (has_nav_menu($menu_name)) {
+            wp_nav_menu(array(
+                'theme_location' => $menu_name,
+                'menu_id' => 'footer-menu',
+                'menu_class' => $menu_class
+            ));
+        } else {
+            echo '<p>Please set a menu to "' . $menu_name . '" in theme settings.</p>';
+        }
         ?>
     </nav>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
--- header.php
+++ header.php
@@ -14,11 +14,22 @@
         </div>
         <nav>
             <?php
-            wp_nav_menu(array(
-                'theme_location' => 'main-menu',
-                'menu_id' => 'main-menu',
-                'menu_class' => 'main-menu'
-            ));
+            $menu_name = 'main-menu';
+            $menu_items = wp_get_nav_menu_items($menu_name);
+            $menu_class = 'main-menu';
+            if ($menu_items && count($menu_items) > 5) {
+                $menu_class .= ' moreThan5';
+            }
+
+            if (has_nav_menu($menu_name)) {
+                wp_nav_menu(array(
+                    'theme_location' => $menu_name,
+                    'menu_id' => 'main-menu',
+                    'menu_class' => $menu_class
+                ));
+            } else {
+                echo '<p>Please set a menu to "' . $menu_name . '" in theme settings.</p>';
+            }
             ?>
         </nav>
     </header>
